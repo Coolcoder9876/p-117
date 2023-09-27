1e170f78054ae178879db5ee04a42434d4b1b40e
Coolcoder9876 <102706727+Coolcoder9876@users.noreply.github.com>
2023-09-27 19:54:21 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+}
+
+function clearCanvas() {
+    background("white");
+}
+
+
+    random_number = random_no = Math.floor((Math.random()*array_1.length)+1);
